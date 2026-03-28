e39f3d3247b876de794f70bb9cb79fd88d7dfcf3
Sanchali <sanchali280705@gmail.com>
2026-03-28 22:49:21 +0530

    chnges

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,6 +1,9 @@
+import { Transaction } from "sequelize";
 import Project from "../models/project.js";
+import { sequelize } from "../config/database.js";
 
 export const createProject = async(req,res)=>{
+    const t = await sequelize.transaction();
     try{
         const {name,description} = req.body;
          console.log("BODY:", req.body);
@@ -15,12 +18,16 @@ export const createProject = async(req,res)=>{
          // ✅ 2. AUTO-ADD creator (manager) to project
         await ProjectMember.create({
             userId: req.user.id,
-            projectId: project.id
-        });
+            projectId: project.id,
+            role : "Manager"
+        }, {transaction : t});
+
+        await t.commit();
         res.status(200).json({
             message : project
         })
     }catch(err){
+        await t.rollback();
         console.log(err);
         return res.status(500).json({
             message : err.message
